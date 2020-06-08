8bec3d3bf3c348bc397923d256e6624dfb6d0f08
boluakins <boluakins@gmail.com>
2020-06-08 19:55:21 +0100

    still working

--- middleware/uploads.js
+++ middleware/uploads.js
@@ -1,7 +1,7 @@
 const multer = require('multer');
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
-        cb(null, './../')
+        cb(null, './../uploads')
     },
     filename: function (req, file, cb) {
         cb(null, new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname)
